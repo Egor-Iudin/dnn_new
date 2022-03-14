6f22e1b69113a09db7337dd1d7d3dd4d63ea77fc
Egor Iudin <iudinegorinfo@gmail.com>
2022-03-14 12:11:38 +0300

    fixed env.sh

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
-source /cvmfs/cms.cern.ch/slc7_amd64_gcc820/external/gcc/8.2.0/etc/profile.d/init.sh
-source /cvmfs/cms.cern.ch/slc7_amd64_gcc820/lcg/root/6.22.04/bin/thisroot.sh
-source /cvmfs/cms.cern.ch/slc7_amd64_gcc820/external/boost/1.72.0/etc/profile.d/init.sh
+source /cvmfs/cms.cern.ch/slc7_amd64_gcc900/external/gcc/9.3.0/etc/profile.d/init.sh
+source /cvmfs/cms.cern.ch/slc7_amd64_gcc900/lcg/root/6.22.08/bin/thisroot.sh
+source /cvmfs/cms.cern.ch/slc7_amd64_gcc900/external/boost/1.75.0/etc/profile.d/init.sh
 
 export PATH=`pwd`/bin:$PATH
